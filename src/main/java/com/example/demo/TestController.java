25ea6c8a44cae73ad25eccae3edaf6d3b33341c3
Becky <becky@jianboqings-MacBook-Pro.local>
2020-04-21 16:40:33 +0800

    minio-demo

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,70 @@
+package com.example.demo;
+
+import com.google.api.client.util.IOUtils;
+import com.jlefebure.spring.boot.minio.MinioException;
+import com.jlefebure.spring.boot.minio.MinioService;
+import io.minio.messages.Item;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/files")
+public class TestController {
+
+    @Autowired
+    private MinioService minioService;
+
+    @GetMapping
+    public List<Item> testMinio() {
+        return minioService.list();
+    }
+
+    @GetMapping("/{object}")
+    public void getObject(@PathVariable("object") String object, HttpServletResponse response) throws MinioException, IOException {
+        InputStream inputStream = minioService.get(Path.of(object));
+        InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
+
+        // Set the content type and attachment header.
+        response.addHeader("Content-disposition", "attachment;filename=" + object);
+        response.setContentType(URLConnection.guessContentTypeFromName(object));
+
+        // Copy the stream to the response's output stream.
+        IOUtils.copy(inputStream, response.getOutputStream());
+        response.flushBuffer();
+    }
+
+    @PostMapping
+    public void addAttachement(@RequestParam("file") MultipartFile file) {
+        Path path = Path.of(file.getOriginalFilename());
+        Map<String, String> header = new HashMap<>();
+        header.put("X-Incident-Id", "C918371984");
+        try {
+            minioService.upload(path, file.getInputStream(), file.getContentType(), header);
+        } catch (MinioException e) {
+            throw new IllegalStateException("The file cannot be upload on the internal storage. Please retry later", e);
+        } catch (IOException e) {
+            throw new IllegalStateException("The file cannot be read", e);
+        }
+    }
+}
+
+
+//@RestController
+//@RequestMapping("/hello")
+//public class TestController {
+//  @GetMapping
+//  public String testMinio() {
+//    return "hello";
+//  }
+//}
